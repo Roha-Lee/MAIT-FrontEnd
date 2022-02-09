559ac7d53164576adddfa598e97d513b2b2d05a0
jinlee <vito9270@naver.com>
2022-02-09 20:48:00 +0900

    로그인 id pw 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-
-
-function App() {
+import React from "react";
+import Login from "./components/login";
+ 
+function App () {
+ 
   return (
-    <div className="App">
-      
+    <div>
+      <Login />
     </div>
-  );
+  )
 }
-
+ 
 export default App;
