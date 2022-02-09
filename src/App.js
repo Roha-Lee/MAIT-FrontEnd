138d0a42c58f306ae5d1d5d3bcf462228030227b
Gn0lee <jinho605@gmail.com>
2022-02-10 00:09:30 +0900

    add : face-landmark-detection 구현 기능 구현 완료. 하지만 웹캠 비율에 따라 감지하는 정도가 달라짐. 비디오 크기가 작아지면 메쉬 그리는 것이 매우 어색해짐. 반응형으로 구현 시 현재 비디오 w,h 비율 유지해야 할 것으로 보임. 현재 App.js에 구현되어 있는 것을 components로 옮겨야함.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,91 @@
-
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import Webcam from "react-webcam";
+import { useEffect, useRef, useState } from "react";
+import { setInterval } from "core-js";
+import { drawMesh } from "./utilites";
 
 
 function App() {
+  // Setup references
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [net,setNet] = useState([]);
+
+  // Load facemesh
+  const runFacemesh = async () => {
+    const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
+    setInterval(() => {
+      detect(net);
+    },1000);
+    // detect(net);
+    // setNet(net);
+  };
+
+  // Detect Function
+  const detect = async(net) => {
+    if(typeof webcamRef.current !== "undefined" && 
+      webcamRef.current !== null && webcamRef.current.video.readyState === 4)
+    {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set Video Width , Height
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      
+      // Set canvas Width, Height
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+      
+      
+      // Make Detections
+      const face = await net.estimateFaces({input:video});
+      console.log(face);
+      
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(()=>{drawMesh(face,ctx)});
+    }
+  };
+  
+  // useEffect(()=>{
+  //   runFacemesh()
+  //   console.log("1")
+  // },[net]);
+  runFacemesh();
+  
   return (
     <div className="App">
-      
+      <Webcam 
+        ref={webcamRef}
+        style={{
+          position : "absolute",
+          marginLeft : "auto",
+          marginRight : "auto",
+          left: 0,
+          right : 0,
+          textAlign : "center",
+          zIndex : 9,
+          width: 320,
+          height: 240,
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position : "absolute",
+          marginLeft : "auto",
+          marginRight : "auto",
+          left: 0,
+          right : 0,
+          textAlign : "center",
+          zIndex : 9,
+          width: 320,
+          height: 240,
+        }}
+      ></canvas>     
     </div>
   );
 }
