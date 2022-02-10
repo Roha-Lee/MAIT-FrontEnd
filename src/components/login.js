c564e87c5c3c9610d9731c86a71140adddc4c106
jinlee <vito9270@naver.com>
2022-02-11 02:17:23 +0900

    Align indents

--- src/components/login.js
+++ src/components/login.js
@@ -6,33 +6,33 @@ axios.defaults.withCredentials = false;
 function Login() {
     const [inputId, setInputId] = useState('')
     const [inputPw, setInputPw] = useState('')
- 
-	// input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
+
+    // input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
     const handleInputId = (e) => {
         setInputId(e.target.value)
     }
- 
+
     const handleInputPw = (e) => {
         setInputPw(e.target.value)
     }
- 
-	// login 버튼 클릭 이벤트
+
+    // login 버튼 클릭 이벤트
     const onClickLogin = () => {
         console.log('click login');
         axios.post("http://192.249.29.38:3001/login", {
-            id : inputId,
-            pw : inputPw
+            id: inputId,
+            pw: inputPw
         }).then(response => console.log(response.data))
     }
- 
+
     return (
         <div>
             <div className="container">
-            <div className="item">EMIT</div>
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item">SIGN UP</div>
-            <div className="item">SIGN IN</div>
+                <div className="item">EMIT</div>
+                <div className="item"></div>
+                <div className="item"></div>
+                <div className="item">SIGN UP</div>
+                <div className="item">SIGN IN</div>
             </div>
             <div>
                 <label htmlFor='input_id'>ID : </label>
@@ -45,10 +45,10 @@ function Login() {
             <div>
                 <button type='button' onClick={onClickLogin}>Login</button>
             </div>
-            <img src="./btn_google_signin_dark_focus_web@2x.png" alt="logo" /> 
+            <img src="./btn_google_signin_dark_focus_web@2x.png" alt="logo" />
             <img src="./kakao_login_medium_narrow.png" alt="kakao" />
         </div>
     )
 }
- 
+
 export default Login;
