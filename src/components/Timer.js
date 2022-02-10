0f21d3b9573600e7a4870ab2e42584b7ce0a81db
Roha-Lee <rohagru@gmail.com>
2022-02-11 02:59:40 +0900

    feature : 타이머 기능 추가  구현 및 클래스 형태로 변경
    기존의 타이머는 start/stop을 누르면 초기화 되었는데, 현재는 누적 시간이 표시되도록 변경함.

---
+++ src/components/Timer.js
@@ -0,0 +1,56 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import {
+  timeStamp, 
+  sendStudyInterval,
+  indexToName
+} from './timerUtils';
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleStopwatch.bind(this);
+  }
+
+  handleStopwatch() {
+    const {subjects, currentTime, currentSubjectId, timerRunning, onChangeCurrentTime, onChangeStudyLog} = this.props;
+    if(!timerRunning){
+      this.startTimeFormatted = timeStamp();
+      this.startTime = Date.now();
+      this.offset = currentTime;
+      this.interval = setInterval(() => {
+        onChangeCurrentTime(this.offset + Date.now() - this.startTime);
+      }, 70)
+    }
+    else {
+      this.endTimeFormatted = timeStamp();
+      clearInterval(this.interval);
+      sendStudyInterval(this.startTimeFormatted, this.endTimeFormatted, currentSubjectId);  
+      onChangeStudyLog(indexToName(subjects, currentSubjectId), currentTime);
+    }
+  }
+
+  render() {
+    const timer = (<h1>
+      <span>{ (this.props.currentTime >= 3600000 ? Math.floor((this.props.currentTime / 3600000) % 24) : Math.floor((this.props.currentTime/ 60000) % 60)).toString().padStart(2, '0') }</span>:
+      <span>{ (this.props.currentTime >= 3600000 ? Math.floor((this.props.currentTime / 60000) % 60) : Math.floor((this.props.currentTime/ 1000) % 60)).toString().padStart(2, '0') }</span>:
+      <span>{ (this.props.currentTime >= 3600000 ? Math.floor((this.props.currentTime / 1000) % 60) : Math.floor((this.props.currentTime % 1000) / 10)).toString().padStart(2, '0') }</span>
+    </h1>);
+    const {
+      onChangeTimerRunning,
+    } = this.props;
+    return ( 
+      <div id = "timer" >
+        {timer}
+        <button onClick = {
+          () => {
+            onChangeTimerRunning(this.props.timerRunning);
+            this.handleStopwatch();
+          }}> 
+          {this.props.timerRunning ? "Stop" : "Start"}
+        </button> 
+      </div>
+    );
+  }
+}
+export default Timer;
