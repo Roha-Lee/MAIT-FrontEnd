50829bd966723ead6e7abebece8e65e60c3c7c19
Roha-Lee <rohagru@gmail.com>
2022-02-11 15:05:39 +0900

    refactor: utils폴더에 함수 모아두기

---
+++ src/utils/AppUtils.js
@@ -0,0 +1,14 @@
+import Axios from 'axios';
+Axios.defaults.withCredentials = false;
+
+async function getAllUserData() {
+  try {
+    const studyInfo = await Axios.get('https://reqres.in/api/users?page=2', {});
+    return studyInfo;
+  } catch(error) {
+    return error;
+  }
+}
+
+
+export {getAllUserData}
---
+++ src/utils/timerUtils.js
@@ -0,0 +1,27 @@
+import Axios from 'axios';
+Axios.defaults.withCredentials = false;
+function sendStudyInterval(startTime, endTime, subjectId) {
+  // console.log('startTime', startTime);
+  // console.log('endTime', endTime);
+  // console.log('subjectId', subjectId); 
+    Axios.post(
+      'http://192.249.31.123:3000/stopwatch',
+      {
+        subject: subjectId,
+        user_id: 1,
+        start_time: JSON.stringify(startTime), 
+        end_time: JSON.stringify(endTime),
+      })
+  }
+  
+function timeStamp(){ 
+  let today = new Date(); 
+  today.setHours(today.getHours() + 9); 
+  return today.toISOString().replace('T', ' ').substring(0, 19); 
+}
+
+function indexToName(obj, idx) {
+  return Object.keys(obj).find((elem) => obj[elem] === idx);
+}
+
+export {sendStudyInterval, timeStamp, indexToName}
