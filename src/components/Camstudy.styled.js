01623a723ac8d09377fc30e9291d852f717398c1
jinlee <vito9270@naver.com>
2022-02-15 16:25:10 +0900

    layout : camstudy layout

---
+++ src/components/Camstudy.styled.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const CamTimerContainer = styled.div`
+    margin: auto;
+    width: 400px;
+    height: 300px;
+    font-size: 1.5rem;
+    border-radius: 15px;
+    background: #EBB057;
+    text-align: center;
+`
+export const CamBoxRank_1 = styled.div`
+    width: 400px;
+    height: 280px;
+    background: red;
+    color: white;
+    text-align: center;
+    font-weight: bolder;
+    margin-top: 20px;
+    border-radius: 10px;
+`
+
+export const CamBoxRank_2 = styled.div`
+    width: 400px;
+    height: 280px;
+    background: #777777;
+    color: white;
+    text-align: center;
+    margin-top: 20px;
+    border-radius: 10px;
+`
+
+export const FlexBox = styled.div`
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    background-color: #DFD8D2;
+    padding: 50px 0 100px 0; 
+`
+
+export const CamContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+`
