1f97b1668237d110824e69460e6775f1f53266a0
Roha-Lee <rohagru@gmail.com>
2022-02-23 16:52:53 +0900

    videocard 만들기 준비작업

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from "./components/Login"
 import Signup from "./components/Signup"
 import Mainpage from './Mainpage';
 import CamStudy from "./components/CamStudy/CamStudy"
+import MaitCamstudy from "./components/MaitCamstudy/MaitCamstudy"
 import Statistics from "./components/Statistics/Statistics"
 import "antd/dist/antd.min.css";
 import "font-awesome/css/font-awesome.min.css";
@@ -18,7 +19,7 @@ function App() {
 
             <Route path="/Signup" element={<Signup/>}/>
             <Route path="/Login" element={<Login/>}/>
-            <Route path="/camstudy" element={<CamStudy/>}/>
+            <Route path="/camstudy" element={<MaitCamstudy/>}/>
             <Route path="Statistics" element={<Statistics/>}/>
             <Route path="/" element={<Mainpage/>}/>
 
---
+++ src/components/MaitCamstudy/MaitCamstudy.js
@@ -0,0 +1,7 @@
+function CamStudy (){
+  return (<div>
+    상단 내비게이션 바 내용 변경 필요.
+  </div>)
+}
+
+export default CamStudy;
