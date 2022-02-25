c4dab2bbe43c6b7bdab33ef661478dfc64ba37cd
chauneng <50446006+chauneng@users.noreply.github.com>
2022-02-25 23:36:53 +0900

    feature: apply pwa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -13,3 +14,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+serviceWorkerRegistration.register();
