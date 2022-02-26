49728845f1bdfba456f2b4f9f57b1d26ac7c1354
Roha-Lee <rohagru@gmail.com>
2022-02-27 04:10:42 +0900

    fix: 통계페이지 스크롤 복구

--- src/components/CamstudyRoom/CamstudyRoom.js
+++ src/components/CamstudyRoom/CamstudyRoom.js
@@ -14,7 +14,6 @@ import { notification } from 'antd';
 import { BellOutlined } from '@ant-design/icons';
 import Bell from "./assets/bell.mp3";
 
-
 const CamstudyRoom = (props) => {
   const currentUser = window.sessionStorage.getItem('currentUser');
   const roomId = window.location.href.split('/camstudyRoom/?roomId=')[1];
